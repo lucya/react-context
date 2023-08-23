050aab08a9c33477bd17567cf7d2f4ac10543267
lucya <neddang@gmail.com>
2023-08-23 12:15:52 +0900

    context hook 예제 커밋

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import Page from './components/Page'
+import { ThemeContext } from './context/ThemeContext';
+import { UserContext } from './context/UserContext';
 function App() {
+  const [isDark, setIsDark] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <UserContext.Provider value="Nature">
+      <ThemeContext.Provider value={{ isDark, setIsDark }}>
+        <Page />
+      </ThemeContext.Provider>
+    </UserContext.Provider>
+  )
 }
 
 export default App;
